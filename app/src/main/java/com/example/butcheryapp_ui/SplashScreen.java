80e8412dd43b410dce3645e846895810158f67ca
SetiadyAnwar <setiadyanwar@gmail.com>
2023-10-19 18:07:32 +0700

    Navigasi

--- app/src/main/java/com/example/butcheryapp_ui/SplashScreen.java
+++ app/src/main/java/com/example/butcheryapp_ui/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, HomePage_NotLogin.class);
                 startActivity(intent);
                 finish();
             }
