eadbdf8a85db38a787ba5eb561c8b2f55af6ec9f
SetiadyAnwar <setiadyanwar@gmail.com>
2023-10-20 19:48:19 +0700

    back_login

--- app/src/main/java/com/example/butcheryapp_ui/LoginPage.java
+++ app/src/main/java/com/example/butcheryapp_ui/LoginPage.java
@@ -1,8 +1,10 @@
 package com.example.butcheryapp_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        ImageButton btnBack = findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, HomePage_NotLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
