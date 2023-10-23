8e583384faa43e364fb1e333a9e33c14281074fd
IPB <admin@ipb.ac.id>
2023-10-23 10:36:06 +0700

    layout daftar mitra started

---
+++ app/src/main/java/com/example/butcheryapp_ui/DaftarMitraDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.butcheryapp_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarMitraDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.daftar_mitra_detail);
+    }
+}
