56a383c883b0ecf67a38ad80dad94d7dc2127fb9
IPB <admin@ipb.ac.id>
2023-11-13 11:33:58 +0700

    implementing fragment switching

--- app/src/main/java/com/example/butcheryapp_ui/AllTokoActivity.java
+++ app/src/main/java/com/example/butcheryapp_ui/AllTokoActivity.java
@@ -3,12 +3,21 @@ package com.example.butcheryapp_ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+
+import com.example.butcheryapp_ui.databinding.ActivityAllTokoBinding;
 
 public class AllTokoActivity extends AppCompatActivity {
+
+    ActivityAllTokoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_toko);
+        setContentView(binding.getRoot());
+        binding = ActivityAllTokoBinding.inflate(getLayoutInflater());
+        RadioButton tokoOption = findViewById(R.id.view_toko);
+        RadioButton produkOption = findViewById(R.id.view_produk);
+        RadioButton etalaseOption = findViewById(R.id.view_etalase);
     }
 }
