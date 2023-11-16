7f9601c9eeef3950d115498e0169a1e48811f2a9
SetiadyAnwar <setiadyanwar@gmail.com>
2023-11-16 19:25:59 +0700

    Manage payment finish

--- app/src/main/java/com/example/butcheryapp_ui/CartPage.java
+++ app/src/main/java/com/example/butcheryapp_ui/CartPage.java
@@ -2,7 +2,11 @@ package com.example.butcheryapp_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class CartPage extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class CartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_page);
+        ImageButton btnbeli = findViewById(R.id.buttonbeli);
+
+        btnbeli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CartPage.this, ManagePaymentPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
