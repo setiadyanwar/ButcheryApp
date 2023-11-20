5ae5e6c2ad41e09dd4d849a2c0ef598464bbb280
IPB <admin@ipb.ac.id>
2023-11-20 11:26:20 +0700

    .

--- app/src/main/java/com/example/butcheryapp_ui/AllTokoActivity.java
+++ app/src/main/java/com/example/butcheryapp_ui/AllTokoActivity.java
@@ -1,6 +1,7 @@
 package com.example.butcheryapp_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.RadioButton;
@@ -15,6 +16,13 @@ public class AllTokoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.toko_views_fragment_container, new TokoFragment());
+        ft.add(R.id.toko_views_fragment_container, new ProdukFragment());
+        ft.add(R.id.toko_views_fragment_container, new EtalaseFragment());
+        ft.commit();
+
         binding = ActivityAllTokoBinding.inflate(getLayoutInflater());
         RadioButton tokoOption = findViewById(R.id.view_toko);
         RadioButton produkOption = findViewById(R.id.view_produk);
--- app/src/main/java/com/example/butcheryapp_ui/TokoFragment.java
+++ app/src/main/java/com/example/butcheryapp_ui/TokoFragment.java
@@ -8,21 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TokoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TokoFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public TokoFragment() {
         // Required empty public constructor
@@ -40,8 +38,6 @@ public class TokoFragment extends Fragment {
     public static TokoFragment newInstance(String param1, String param2) {
         TokoFragment fragment = new TokoFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +45,19 @@ public class TokoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toko, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_toko, container, false);
+
+        ImageSlider imageSlider = view.findViewById(R.id.imageSlider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.banner1,null, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner2,null, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner3,null, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.banner4,null, ScaleTypes.FIT));
+
+        return view;
     }
 }
