9f06fc6c9dddd0cbc4324a14b0788c712d759039
IPB <admin@ipb.ac.id>
2023-11-27 09:56:24 +0700

    feature: etalase view layout fin

--- app/src/main/java/com/example/butcheryapp_ui/TokoFragment.java
+++ app/src/main/java/com/example/butcheryapp_ui/TokoFragment.java
@@ -15,27 +15,13 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TokoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TokoFragment extends Fragment {
 
     public TokoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TokoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TokoFragment newInstance(String param1, String param2) {
+    public static TokoFragment newInstance() {
         TokoFragment fragment = new TokoFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
